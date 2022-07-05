c7043811f26a7c23b932cba742c2cbf8103baf8a
malickruru <rubs99rstd@gmail.com>
2022-07-05 13:36:12 +0000

    firebase auth config

---
+++ public/script/firebase/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfu865SxCMse2ox-CyvhQjN0Ycvq5hgnY",
+    authDomain: "ifran-6dbe6.firebaseapp.com",
+    projectId: "ifran-6dbe6",
+    storageBucket: "ifran-6dbe6.appspot.com",
+    messagingSenderId: "892500073140",
+    appId: "1:892500073140:web:cf0a949751386b748a55e1",
+    measurementId: "G-MJHSEBSPVL"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
